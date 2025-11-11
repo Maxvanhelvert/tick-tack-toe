816dda86d4edf0c2a2029c6be506435648576aac
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-11-12 07:06:15 +1100

    initial commit, psudo code for gameflow

---
+++ main.rb
@@ -0,0 +1,13 @@
+# Build a tic-tac-toe game on the command line where two human players can play against each other
+# and the board is displayed in between turns.
+# main.rb that handles gameplay
+# a board class ?what does it hold/do?
+
+# gameplay:
+# game gets started
+# 1 or 2 players get selected (if 1 the other will be computer)
+# ?each player gets its own color?
+# player 1 gets first turn, chooses their choice
+# player 2 gets a turn
+# after every turn, check for a win condition.
+# if win condition, end game and declare winner.
